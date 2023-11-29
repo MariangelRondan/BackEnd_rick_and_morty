927ac7f6824c4f3f52050a64caf75c26a619d3ae
MariangelRondan <rondanmariangel@gmail.com>
2023-11-29 14:36:02 -0300

    back for deploy

--- src/controller/getCharById.js
+++ src/controller/getCharById.js
@@ -1,29 +1,10 @@
-// const axios = require("axios");
-// const URL = "https://rickandmortyapi.com/api/character/";
+const dotenv = require("dotenv");
+dotenv.config();
+const axios = require("axios");
 
-// const getCharById = (req, res) => {
-//   const { id } = req.params; //req.params es un objeto
-//   console.log(id);
-//   axios(URL + id)
-//     .then(({ data }) => {
-//       const { name, gender, species, origin, image, status } = data;
-//       const char = { name, gender, species, origin, image, status, id };
-//       if (!char.name) {
-//         res.status(404).send("Not found");
-//       } else {
-//         res.status(200).json(char);
-//       }
-//     })
-//     .catch((err) => {
-//       return res.status(500).send(err.message);
-//     });
-// };
-
-// module.exports = getCharById;
+const URL = process.env.URL_API;
 
 //con ASYN AWAIT
-const axios = require("axios");
-const URL = "https://rickandmortyapi.com/api/character/";
 
 const getCharById = async (req, res) => {
   const { id } = req.params; //req.params es un objeto
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const server = require("./App");
 const { conn } = require("./DB_connection");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
