07a26ecca6c63a814211e7b5f94a76865ae1f10f
MariangelRondan <rondanmariangel@gmail.com>
2024-01-28 18:21:40 -0300

    deploy

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -3,11 +3,17 @@ const { Sequelize } = require("sequelize");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const FavoriteModel = require("./models/Favorite");
 const UserModel = require("./models/User");
+const dataBaseDeploy = process.env.DATA_BASE_DEPLOY;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
-  { logging: false, native: false }
-);
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+//   { logging: false, native: false }
+// );
+
+const sequelize = new Sequelize(dataBaseDeploy, {
+  logging: false,
+  native: false,
+});
 
 FavoriteModel(sequelize);
 
