40bb38b11495d41200ebb4f62bd1e64fcb05ff57
Mariangel <rondanmariangel@gmail.com>
2024-02-01 00:45:54 -0300

    bback

--- src/controller/postUser.js
+++ src/controller/postUser.js
@@ -3,44 +3,18 @@ const { User } = require("../DB_connection");
 const postUser = async (req, res) => {
   const { name, lastname, dateOfBirth, email, password } = req.body;
   try {
-    if (!name || !lastname || !dateOfBirth || !email || !password) {
-      return res.status(400).send("missing data");
-    }
-    const [user, isCreated] = await User.findOrCreate({
-      where: {
-        name: name,
-        lastname: lastname,
-        dateOfBirth: dateOfBirth,
-        email: email,
-        password: password,
-      },
+   
+    const user = await User.create({
+      name,
+      lastname,
+      dateOfBirth,
+      email,
+      password,
     });
+    
     res.status(201).send({ user, created: isCreated });
   } catch (error) {
-    res.status(500).json({ error: error.menssage });
   }
 };
 
 module.exports = postUser;
-
-// const { User } = require("../DB_connection");
-
-// const postUser = async (req, res) => {
-//   const { email, password} = req.body;
-//   try {
-//     if (!email || !password) {
-//       return res.status(400).send("missing data");
-//     }
-//     const [user, isCreated] = await User.findOrCreate({
-//       where: {
-//         email: email,
-//         password: password,
-//       },
-//     });
-//     res.status(201).send({ user, created: isCreated });
-//   } catch (error) {
-//     res.status(500).json({ error: error.menssage });
-//   }
-// };
-
-// module.exports = postUser;
