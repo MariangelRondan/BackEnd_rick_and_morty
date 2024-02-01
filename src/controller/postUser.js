98948acfd7b66ffa81f5beba5cdabad8e71d6cae
Mariangel <rondanmariangel@gmail.com>
2024-02-01 12:31:21 -0300

    finish

--- src/controller/postUser.js
+++ src/controller/postUser.js
@@ -1,20 +1,22 @@
 const { User } = require("../DB_connection");
 
-const postUser = async (req, res) => {
-  const { name, lastname, dateOfBirth, email, password } = req.body;
-  try {
-   
-    const user = await User.create({
-      name,
-      lastname,
-      dateOfBirth,
-      email,
-      password,
-    });
-    
-    res.status(201).send({ user, created: isCreated });
-  } catch (error) {
-  }
-};
+const postUser = async(req,res) => {
+  const {name, lastname, email, password} = req.body;
+try {
+  if(!name || !lastname || !email || !password) return res.status(400).send('Missing data')
+  const [user, isCreated]= await User.findOrCreate({
+      where: {
+          name: name,
+          lastname: lastname,
+          email: email,
+          password: password,
+      }
+  })
+  res.status(201).send({user, created: isCreated});
+} catch (error) {
+  res.status(500).json({error: error.message})
+}
+}
+
 
 module.exports = postUser;
